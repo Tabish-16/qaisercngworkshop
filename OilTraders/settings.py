300bbe79bd1d7a29fc5432821369dcf5f218a192
Tabish-16 <79455085+Tabish-16@users.noreply.github.com>
2024-09-12 15:21:15 +0500

    Update settings.py

--- OilTraders/settings.py
+++ OilTraders/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'OilTraders',
+    'corsheaders',
     # 'background_task',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,15 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+    "https://qaisercngworkshop.onrender.com",
+]
+CORS_ALLOW_ALL_ORIGINS = True
+
+
 
 ROOT_URLCONF = 'OilTraders.urls'
 
